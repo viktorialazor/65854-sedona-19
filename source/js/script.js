96970425c009a37d70439fc6d08bbd1002125767
viktoria.lazor@gmail.com <viktoria.lazor@gmail.com>
2020-04-18 16:04:52 +0300

    Правки для начала верстки с препроцессором

--- source/js/script.js
+++ source/js/script.js
@@ -4,13 +4,6 @@ var navigation = document.querySelector(".js-navigation");
 button.addEventListener("click", function(evt){
   evt.preventDefault();
 
-  if(navigation.classList.contains("navigation--show")) {
-    navigation.classList.remove("navigation--show");
-    navigation.classList.add("navigation--hide");
-    button.classList.remove("header__button--active");
-  } else {
-    navigation.classList.remove("navigation--hide");
-    navigation.classList.add("navigation--show");
-    button.classList.add("header__button--active");
-  }
+  navigation.classList.toggle("navigation--show");
+  button.classList.toggle("header__button--active");
 });
