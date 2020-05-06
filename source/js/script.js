8bfed60951719cfa50894fc12b731559357895c5
viktoria.lazor@gmail.com <viktoria.lazor@gmail.com>
2020-05-06 15:12:52 +0300

    Продолжение верстки личного проекта

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,13 @@
 var button = document.querySelector(".js-menu-button");
 var navigation = document.querySelector(".js-navigation");
 
+navigation.classList.add("navigation--hide");
+button.classList.remove("header__button--hide");
+button.classList.add("header__button--closed");
+
 button.addEventListener("click", function(evt){
   evt.preventDefault();
 
-  navigation.classList.toggle("navigation--show");
-  button.classList.toggle("header__button--active");
+  navigation.classList.toggle("navigation--hide");
+  button.classList.toggle("header__button--closed");
 });
